e69fc9b5a000a04eeef19bce801d8929138230b7
Ömer Faruk Aydın <o.faduzce@gmail.com>
2023-05-20 21:25:42 +0300

    Project Files Uploaded

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+//Student ID: B201202023
+//Name: Ömer Faruk
+//Surname: Aydın
+//Course Name: Software Verification and Validation
+//Homework 1
+//https://github.com/ofaydn/TDDExample
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"10,2,5", "10,4,2.5", "12.5,5,2.5", "10,2.5,4", "12.5,2.5,5"})
+    public void ParamTest(float a,float b,float expected){
+        assertEquals(expected,Calculator.divide(a,b));
+    }
+
+}
